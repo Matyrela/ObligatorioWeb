a573a601d4e00e6220c8aad8b788aea432783436
PabloConstantinoUCU <PABLO.CONSTANTINO@correo.ucu.edu.uy>
2023-11-15 17:27:16 -0300

    cambios en game handler y activity.ts

--- Angular - PromptsPeladas/src/app/clases/activity.ts
+++ Angular - PromptsPeladas/src/app/clases/activity.ts
@@ -1,6 +1,17 @@
-export interface Activity {
+export class Activity {
     _id: string;
     player_id: string;
     description: string;
     enabled: boolean;
+
+    constructor(id: string, playerName: string, description: string) {
+        this._id = id;
+        this.player_id = playerName;
+        this.description = description;
+        this.enabled = true;
+    }
+
+    public disableActivity() {
+        this.enabled = false;
+    }
 }
