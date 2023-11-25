ae4e2d50506d14c96371d844a43002fa25f6641c
Enzoporte <enzocantoni03@gmail.com>
2023-11-25 20:29:25 -0300

    Clase Auth

--- Angular - PromptsPeladas/src/app/enviroment.ts
+++ Angular - PromptsPeladas/src/app/enviroment.ts
@@ -1,6 +1,6 @@
 export const env = {
     production: false,
-    angularURL: 'http://192.168.1.3:4200/',
-    baseURL: 'http://192.168.1.3:7777/api',
-    WebSocket: "ws://192.168.1.3:7777/games/ws/" 
+    angularURL: 'http://192.168.1.42:4200/',
+    baseURL: 'http://192.168.1.42:7777/api',
+    WebSocket: "ws://192.168.1.42:7777/games/ws/" 
 }
